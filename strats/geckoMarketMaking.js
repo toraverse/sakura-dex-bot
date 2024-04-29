8aa1221a8dfc69a179e41810cc53049f1aeee9d2
ashish <ashish24rawat@gmail.com>
2024-04-29 19:19:46 +0400

    smarter mm

--- strats/geckoMarketMaking.js
+++ strats/geckoMarketMaking.js
@@ -29,13 +29,13 @@ class GeckoMarketMaking {
             console.error("Failed to fetch price from CoinGecko.");
             return;
         }
-        console.log(`Fetched current price: ${floatPrice} for ${this.tegroConnector.marketSymbol}`);
+        console.log('\x1b[36m%s\x1b[0m', `Fetched current price: ${floatPrice} for ${this.tegroConnector.marketSymbol}`);
         return floatPrice * 10 ** this.tegroConnector.quoteDecimals;
     }
 
     calculatePriceLevels(basePrice, percentages, type) {
         const priceLevels = percentages.map(percentage => BigInt(Math.floor(basePrice * (1 + (type === 'sell' ? 1 : -1) * percentage / 100))));
-        console.log(`Calculated ${type} price levels for ${this.tegroConnector.marketSymbol}: ${priceLevels}`);
+        //console.log(`Calculated ${type} price levels for ${this.tegroConnector.marketSymbol}: ${Number(priceLevels)/10**this.tegroConnector.quoteDecimals}`);
         return priceLevels;
     }
 
@@ -56,23 +56,49 @@ class GeckoMarketMaking {
         return quantities;
     }
 
-    async cancelOrders() {
-        const openOrders = await this.tegroConnector.getActiveOrders();
-        console.log(`Fetched ${openOrders.length} active orders from the market.`);
+    async cancelOrders(priceLevels, type) {
+        let openOrders = await this.tegroConnector.getActiveOrders();
+        let filteredOrders = this.tegroConnector.filterActiveOrdersBySide(openOrders, type);
+        const initialPriceLevels = priceLevels.length;
 
-        // Cancel all existing orders of the same type
-        for (const order of openOrders) {
-            console.log(`Cancelling order ${order.orderId} for ${this.tegroConnector.marketSymbol}...`);
-            await this.tegroConnector.cancelOrder(order.orderId).catch(err => console.error(`Failed to cancel order ${order.orderId}:`, err));
+        let minPrice;
+        let maxPrice;
+
+        if (type === 'buy') {
+            minPrice = priceLevels[priceLevels.length - 1];
+            maxPrice = priceLevels[0];
+        } else {
+            minPrice = priceLevels[0];
+            maxPrice = priceLevels[priceLevels.length - 1];
         }
+
+        let ordersCancelled = 0;
+
+        for (const order of filteredOrders) {
+            const orderPrice = BigInt(order.pricePrecision);
+            if (orderPrice < minPrice || orderPrice > maxPrice) {
+                console.log(`Cancelling ${type} order for ${Number(order.price)} because it is outside of the price range [${Number(minPrice) / 10 ** this.tegroConnector.quoteDecimals}, ${Number(maxPrice) / 10 ** this.tegroConnector.quoteDecimals}]`);
+                await this.tegroConnector.cancelOrder(order.orderId).catch(err => console.error(`Failed to cancel order ${order.orderId}: `, err));
+                ordersCancelled++;
+            }
+            else {
+                priceLevels.shift();
+                console.log(`NOT cancelling order ${order.price} because it is inside of the price range [${Number(minPrice) / 10 ** this.tegroConnector.quoteDecimals}, ${Number(maxPrice) / 10 ** this.tegroConnector.quoteDecimals}]`);
+            }
+        }
+
+        console.log(`\x1b[33m%s\x1b[0m`, `${type} Orders to create: ${priceLevels.length}`);
+        return priceLevels;
     }
 
     async manageOrders(priceLevels, quantities, type) {
+        let ordersToPlace = priceLevels.length;
+
         // Place new orders based on type
-        for (let i = 0; i < priceLevels.length; i++) {
-            console.log(`Placing new ${type} order at price ${priceLevels[i]} with quantity ${quantities[i]} for ${this.tegroConnector.marketSymbol}...`);
+        for (let i = 0; i < ordersToPlace; i++) {
+            console.log(`\x1b[32m`, `Placing new ${type} order at price ${priceLevels[i]} with quantity ${quantities[i]} for ${this.tegroConnector.marketSymbol}...`);
             await this.tegroConnector.placeOrder(type, priceLevels[i], quantities[i])
-                .catch(err => console.error(`Failed to place ${type} order at ${priceLevels[i]}:${quantities[i]}`, err));
+                .catch(err => console.error(`Failed to place ${type} order at ${priceLevels[i]}:${quantities[i]} `, err));
         }
     }
 
@@ -93,8 +119,8 @@ class GeckoMarketMaking {
     async runMM() {
         const basePrice = await this.fetchCurrentPriceInPrecision();
 
-        const buyPriceLevels = this.calculatePriceLevels(basePrice, this.priceStepLevels, 'buy');
-        const sellPriceLevels = this.calculatePriceLevels(basePrice, this.priceStepLevels, 'sell');
+        let buyPriceLevels = this.calculatePriceLevels(basePrice, this.priceStepLevels, 'buy');
+        let sellPriceLevels = this.calculatePriceLevels(basePrice, this.priceStepLevels, 'sell');
 
         let quoteBalance = await this.getQuoteBalance();
         let baseBalance = await this.getBaseBalance();
@@ -107,12 +133,13 @@ class GeckoMarketMaking {
         const buyQuantities = this.calculateQuantities(buyPriceLevels, quoteBalance, this.walletAllocation);
         const sellQuantities = this.calculateSellQuantities(sellPriceLevels, baseBalance, this.walletAllocation);
 
-        await this.cancelOrders();
+        buyPriceLevels = await this.cancelOrders(buyPriceLevels, 'buy');
+        sellPriceLevels = await this.cancelOrders(sellPriceLevels, 'sell');
 
         await this.manageOrders(buyPriceLevels, buyQuantities, 'buy');
         await this.manageOrders(sellPriceLevels, sellQuantities, 'sell');
 
-        setTimeout(() => this.runMM(), this.orderRefreshFrequency); // Correctly bind `this` to the instance
+        setTimeout(() => this.runMM(), this.orderRefreshFrequency);
     }
 }
 
